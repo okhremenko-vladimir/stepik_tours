e2f2d15eab9281eb69c6f6d8144307c1bfca3559
okhremenko-vladimir <vladimir.okhremenko@gmail.com>
2022-08-02 07:34:38 +0700

    init project

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from tours.views import main_view, departure_view, tour_view, custom_handler404, custom_handler500
+
+
+handler404 = custom_handler404
+handler500 = custom_handler500
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view, name='main'),
+    path('departure/<str:departure>/', departure_view, name='departure'),
+    path('tour/<int:id>/', tour_view, name='tour')
 ]
---
+++ tours/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseServerError
+
+
+def main_view(request):
+    return render(request, 'tours/index.html')
+
+
+def departure_view(request, departure):
+    return render(request, 'tours/departure.html')
+
+
+def tour_view(request, id):
+    return render(request, 'tours/tour.html')
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Ошибка сервера!')
