b316ea94c9d6e9202b1b62659b275acf5c0e88e4
okhremenko-vladimir <vladimir.okhremenko@gmail.com>
2022-08-05 16:39:59 +0700

    added context for tour

--- tours/views.py
+++ tours/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, HttpResponseServerError
 
-from tours.data import tours
+from tours.data import departures, tours
 
 
 def main_view(request):
@@ -13,7 +13,15 @@ def departure_view(request, departure):
 
 
 def tour_view(request, tour_id):
-    return render(request, 'tours/tour.html')
+    title = tours[tour_id]['title'] + ' ' + '★' * int(tours[tour_id]['stars'])
+    tour_info = tours[tour_id]['country'] + ' из ' + departures[tours[tour_id]['departure']]
+    tour_info += ' ' + str(tours[tour_id]['nights']) + ' ночей'
+    tour_about = tours[tour_id]['description']
+    price = tours[tour_id]['price']
+    link_picture = tours[tour_id]['picture']
+    context = {'title': title, 'tour_info': tour_info, 'tour_about': tour_about,
+               'price': price, 'link_picture': link_picture}
+    return render(request, 'tours/tour.html', context)
 
 
 def custom_handler404(request, exception):
